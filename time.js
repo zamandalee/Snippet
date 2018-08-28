693378b0da83a15f745c6325aa1b0231b6ca0c4e
Amanda <a@zalee.me>
2018-08-28 09:16:02 -0700

    Change current time

--- time.js
+++ time.js
@@ -1,7 +1,7 @@
 function currentTime() {
   let timeH1 = document.getElementById("currentTime");
   let time = new Date();
-  timeH1.innerHTML = time.toLocaleTimeString().slice(0,5);
+  timeH1.innerHTML = time.getHours() + ":" + time.getMinutes();
 }
 
 currentTime();

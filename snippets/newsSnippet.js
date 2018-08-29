32c2246ab781affee9f3910a8d4eddb7bcf3c085
Amanda <a@zalee.me>
2018-08-28 22:32:47 -0700

    Add styling and JS for synchronized dot indicators with automatic carousel

--- snippets/newsSnippet.js
+++ snippets/newsSnippet.js
@@ -1,3 +1,4 @@
+// fetching top news from NYT "home"
 let url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 
 url += '?' + $.param({
@@ -24,20 +25,44 @@ function fiveHomeNews(result) {
     let abstract = document.getElementById(i + "-abstract");
 
     headline.innerHTML = result[i].title;
+    headline.href = result[i].url;
     multimedia.src = result[i].multimedia[4].url;
     copyright.innerHTML = result[i].multimedia[4].copyright;
     abstract.innerHTML = result[i].abstract;
   }
 }
 
+// news article carouselling
+// let slideIndex = 0;
+// carousel();
+//
+// function carousel(n) {
+//   let articles = document.getElementsByClassName("news-article");
+//
+//   for (let i = 0; i < articles.length; i++) {
+//     articles[i].style.display = "none";
+//   }
+//
+//   slideIndex++;
+//
+//   if (slideIndex > articles.length) {
+//     slideIndex = 1;
+//   }
+//
+//   articles[slideIndex - 1].style.display = "block";
+//   setTimeout(carousel, 4500);
+// }
+
 let slideIndex = 0;
-carousel();
+showSlides();
 
-function carousel() {
-    let articles = document.getElementsByClassName("news-article");
+function showSlides() {
+    let i;
+    const articles = document.getElementsByClassName("news-article");
+    const dots = document.getElementsByClassName("dot");
 
-    for (let i = 0; i < articles.length; i++) {
-      articles[i].style.display = "none";
+    for (i = 0; i < articles.length; i++) {
+       articles[i].style.display = "none";
     }
 
     slideIndex++;
@@ -46,6 +71,11 @@ function carousel() {
       slideIndex = 1;
     }
 
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
     articles[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 4500);
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000);
 }

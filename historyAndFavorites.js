4651f92341f89810a8633488e2ef09b02a37a549
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-09-02 14:28:24 -0700

    Add modal design and x button

--- historyAndFavorites.js
+++ historyAndFavorites.js
@@ -1,4 +1,9 @@
 /*eslint no-undef: 0*/
+
+const favoriteButton = document.getElementById('favorites');
+const modal = document.getElementsByClassName('black-modal')[0];
+const modalX = document.getElementById('black-modal-x');
+
 Array.from(hearts).forEach( heart => {
   heart.addEventListener('click', (event) => {
     console.log(date);
@@ -19,4 +24,12 @@ Array.from(hearts).forEach( heart => {
 
     event.stopPropagation();
   });
+});
+
+favoriteButton.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+});
+
+modalX.addEventListener('click', () => {
+  modal.classList.add('hidden');
 });

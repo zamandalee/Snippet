b49bc0770aeb626aae8ff7a27409591938e5e49d
Amanda <a@zalee.me>
2020-07-04 18:53:27 -0400

    Secure Oxford Dictionary API key

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
-/*eslint no-undef: 0*/
+import { config } from './config.js'
 
 const defaultPreferences = {
   name: ''
 };
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({preferences: defaultPreferences, todos: {}}, function() {
+  chrome.storage.sync.set({ preferences: defaultPreferences, todos: {} }, function() {
     console.log('Default preferences syced.');
   });
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.update({url: "https://zamandalee.github.io/Snippet/"});
+  chrome.tabs.update({ url: "https://zamandalee.github.io/Snippet/"} );
   window.close();
 });
 
@@ -20,11 +20,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     case 'getVocabWord':
       // ajax request to oxford dictionary for vocabSnippet
       $.ajax({
-        url: `https://od-api.oxforddictionaries.com/api/v1/entries/en/${request.word}`,
+        url: `https://od-api.oxforddictionaries.com:443/api/v2/entries/en-us/${request.word}`,
         method: 'GET',
         headers: {
-          'app_id': '2b9cea8c',
-          'app_key': 'd1ab5e609892b9e47be76b2fee2d40ba'
+          'app_id': config.OXFORD_DICT_APP_ID,
+          'app_key': config.OXFORD_DICT_APP_KEY
         },
       }).done( result => {
         sendResponse({result: result.results[0]});
@@ -32,3 +32,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
   return true; //necessary for sendResponse() to be valid
 });
+
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   debugger;
+//   switch( request.type ) {
+//     case 'getVocabWord':
+//       // ajax request to oxford dictionary for vocabSnippet
+//       debugger;
+//       $.ajax({
+//         url: `https://od-api.oxforddictionaries.com:443/api/v2/entries/en-us/${request.word}`,
+//         method: 'GET',
+//         headers: {
+//           'app_id': "4d02697c",
+//           'app_key': "bb507e921eece6a19bba40e6f321026a"
+//         },
+//       }).done( result => {
+//         sendResponse({result: result.results[0]});
+//       });
+//   }
+//   return true; //necessary for sendResponse() to be valid
+// });
